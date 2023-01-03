b7a67004729da113ff2ff97bd6922697987cf0f4
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-01-03 16:37:36 +0800

    Request 1: setup basic controller and route test

---
+++ spec/routing/cashflow_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe CashflowController, type: :routing do
+  it 'routes to index when hit root path' do
+    expect(get('/')).to route_to('cashflow#index')
+  end
+
+  it 'routes to index when hit /cashflow' do
+    expect(get('/cashflow')).to route_to('cashflow#index')
+  end
+
+  it 'does not support other generic routes' do
+    expect(get('/cashflow/1')).not_to be_routable
+    expect(post('/cashflow')).not_to be_routable
+    expect(delete('/cashflow/1')).not_to be_routable
+    expect(put('/cashflow/1')).not_to be_routable
+    expect(patch('/cashflow/1')).not_to be_routable
+  end
+end

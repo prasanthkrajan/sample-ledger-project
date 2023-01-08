f215dbfadd2bdd6d3d7661a855b11c56c9a4bb09
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-01-08 22:59:57 +0800

    Request 4: add ledger entries controller and relevant routes and specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'ledgers#my_ledger'
   get '/my_ledger', to: 'ledgers#my_ledger'
   post 'export', to: 'ledgers#export'
+
+  resources :ledgers, only: [:new, :create, :index, :show]
+  resources :ledger_entries, only: [:new, :create, :index]
 end
--- spec/routing/ledgers_spec.rb
+++ spec/routing/ledgers_spec.rb
@@ -2,23 +2,26 @@ require "rails_helper"
 
 RSpec.describe LedgersController, type: :routing do
   it 'routes to my_ledger when hit root path' do
-    expect(get('/')).to route_to('ledgers#my_ledger')
+    expect(get('/')).to route_to({controller: 'ledgers', action: 'my_ledger'})
   end
 
   it 'routes to my_ledger when hit /ledgers' do
-    expect(get('/my_ledger')).to route_to('ledgers#my_ledger')
+    expect(get('/my_ledger')).to route_to({controller: 'ledgers', action: 'my_ledger'})
   end
 
   it 'routes to export when post to /export' do
-    expect(post('/export')).to route_to('ledgers#export')
+    expect(post('/export')).to route_to({controller: 'ledgers', action: 'export'})
+  end
+
+  it 'routes to supported generic routes' do
+    expect(get('/ledgers/new')).to route_to({controller: 'ledgers', action: 'new'})
+    expect(post('/ledgers')).to route_to({controller: 'ledgers', action: 'create'})
+    expect(get('/ledgers')).to route_to({controller: 'ledgers', action: 'index'})
+    expect(get('/ledgers/1')).to route_to({controller: 'ledgers', action: 'show', id: '1'})
   end
 
   it 'does not support other generic routes' do
     expect(get('/export')).not_to be_routable
-    expect(get('/ledgers')).not_to be_routable
-    expect(get('/ledgers/new')).not_to be_routable
-    expect(get('/ledgers/1')).not_to be_routable
-    expect(post('/ledgers')).not_to be_routable
     expect(delete('/ledgers/1')).not_to be_routable
     expect(put('/ledgers/1')).not_to be_routable
     expect(patch('/ledgers/1')).not_to be_routable

b009d4b0a818c89cc8959d9d9a431796fb8c8bcd
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-01-08 23:37:32 +0800

    Request 4: add basic model specs

--- spec/models/ledger_entry_spec.rb
+++ spec/models/ledger_entry_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe LedgerEntry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "belongs to ledger" do
+    should respond_to(:ledger)
+    t = LedgerEntry.reflect_on_association(:ledger)
+    expect(t.macro).to eq(:belongs_to)
+  end
 end
--- spec/models/ledger_spec.rb
+++ spec/models/ledger_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Ledger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a title' do
+    ledger = Ledger.new(title: nil)
+    expect(ledger).to_not be_valid
+  end
+
+  it "has many ledger entries" do
+    should respond_to(:ledger_entries)
+  end
 end
